326329381dea9cda1db98e5aa7964bba2a7cbe28
Joseph DALY <joseph300905@gmail.com>
2023-03-04 17:20:24 +0100

    Tailwind

---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<script lang="ts"></script>
+
+<div class="flex justify-between items-center px-32 py-4 border-b border-gray-200">
+  <div class="flex items-center">
+    <a href="/"><h3 class="text-3xl font-bold cursor-pointer">ENTLite</h3></a>
+    <ul class="ml-16 space-x-8">
+      <li>Foo</li>
+      <li>Bar</li>
+      <li>Baz</li>
+      <li>Qux</li>
+    </ul>
+  </div>
+  <div class="space-x-4">
+    <a href="/auth">Connexion</a>
+  </div>
+</div>
+
+<style>
+  li {
+    @apply inline;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import Header from "../components/Header.svelte";
 	import type { PageServerData } from "./$types";
 
 	export let data: PageServerData;
 </script>
 
+<Header />
 <h1>Bonjour {data.user.firstName}</h1>
