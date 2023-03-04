1436aab704e0a9b92eed3448abaace1c2840be54
Joseph DALY <joseph300905@gmail.com>
2023-03-04 23:48:00 +0100

    Messages page design

--- src/routes/message/+page.svelte
+++ src/routes/message/+page.svelte
@@ -4,14 +4,11 @@
   export let data: PageData;
 </script>
 
-<div class="px-24 py-8 space-y-4">
+<div class="">
   {#each data.messages as msg}
-    <div class="border border-slate-200 rounded-md p-4">
-      <div class="flex items-center justify-between">
-        <h3>{msg.subject}</h3>
-        {#if msg.unread}
-          <div class="bg-indigo-400 rounded-full p-2" />
-        {/if}
+  <div class={`cursor-pointer border-b border-slate-300 px-4 py-2 ${msg.unread ? 'bg-slate-100 border-slate-200 font-bold' : 'bg-slate-200 border-slate-300'}`}>
+      <div class={`flex items-center justify-between`}>
+        <h3>{msg.subject.slice(0, 64).trim() + (msg.subject.length > 64 ? '...' : '')}</h3>
       </div>
     </div>
   {/each}
